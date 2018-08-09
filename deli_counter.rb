40f5518d3a249301fcd2cc9537e7a22b325a3bcd
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-09 08:01:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,36 +1,112 @@
-class Deli_counter
-
-  attr_accessor :person, :katz_deli
 
 
+katz_deli = []
 
-  def initialize
-    @person = person
-    @katz_deli = []
-  end
+    # line = katz_deli.count.to_i
 
-  def take_a_number(katz_deli, person)
-    if deli_counter.new
-      deli_counter.new.count times do
-      @katx_deli << person && katz_deli.count.to_i + 1
-    else
-      return "Welcome, #{person}. You are number #{katz_deli.count} in line."
-    end
-  end
+  def line(katz_deli)
 
-def line
-  if @katz_deli.empty?
-    return "The line is currently empty."
-  else
-    return "The line is currently: #{@katz_deli}"
-  end
+    # if katz_deli.empty?
+    #   puts "The line is currently empty."
+    # else
+      # katz_deli.each_with_index {|person, index| puts "The line is currently: #{index.next}. #{katz_deli}"}
+      # if katz_deli > 0
+#
+        # if katz_deli.length == 0
+
+
+            # katz_deli[index].length > 0
+
+            # indexplusone = katz_deli[index] + 1
+
+          # katz_deli.each.with_index do |katz_deli, index|
+            # return deli_counter
+
+# .each.with_index
+            # string2 = '
+
+                          # indexplusone = index + 1
+
+                                                              # print katz_deli.map.with_index(1){|katz_deli,index|}
+                                                              # # index = katz_deli[index]
+                                                              # katz_deli.each_with_index do |katz_deli, index|
+                                                              #   puts "The line is currently: #{index+1}. #{katz_deli}"
+
+#                                                               katz_deli.each do |index, katz_deli|
+#
+#                                                                 puts "The line is currently:"
+#                                                                 puts index. katz_deli
+#                                                                 katz_deli.push(person)
+# index =+ 1
+
+    katz_deli = []
+
+
+          if  katz_deli.length != 0
+          katz_deli.each.with_index(1) do |katz_deli, index|
+          katz_deli.push("#{index}. #{katz_deli}")
+          puts "The line is currently: #{katz_deli.join(" ")}"
+        end
+          if katz_deli.length == 0
+          puts "The line is currently empty."
+        end
+        end
 end
+      #
+      # print "The line is currently:"
+      # print katz_deli.each_with_index {|person, index|   " #{index +1}. #{person}" }
+
+    # end
+
+
+# end
+# print "#{katz_deli} #{index+1}"
+
+                                    #  total << "#{index}.#{katz_deli}"}
+
+            #   # print katz_deli.length, katz_deli
+            # end
+            # if katz_deli.empty?
+            #   puts "The line is currently empty."
+            #
+
+            # katz_deli.each_index.select{|index| katz_deli[index]}
+
+            # puts string
+
+# end
+
+      #   end
+      # end
+# end
+
+    def take_a_number(katz_deli, person)
+      # katz_deli.push(person)
+      # # if Deli_counter.new += 1
+      #   person = katz_deli.count.to_i
+              # katz_deli << person
+              katz_deli.push(person)
+        # puts katz_deli        katz_deli.each.with_index do |katz_dell, index|
+#
+        # katz_deli.each_with_index do |person, index|
+
+        puts "Welcome, #{katz_deli[-1]}. You are number #{katz_deli.length} in line."
+
+    # end
+
+    end
+      # puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]} 4. #{katz_deli[3]} 5. #{katz_deli[4]} 6. #{katz_deli[5]} 7. #{katz_deli[6]} 8. #{katz_deli[7]} 9. #{katz_deli[8]} 10. #{katz_deli[9]}"
+        # end
+    # puts current_line
+  #   end
+  # end
+# # end
+# end
 
   def now_serving(katz_deli)
-    if @katz_deli.empty?
-      return "There is nobody waiting to be served!"
+    if katz_deli.empty?
+      puts "There is nobody waiting to be served!"
     else
-      return "Currently serving #{@katz_deli.shift}."
+      puts "Currently serving #{katz_deli.shift}."
     end
   end
-end
